a103f02d4150c02178dd58ff3888a16ab6f293e0
R Rafiou Khan Eshan <140967916+c683r4ss4ss1n@users.noreply.github.com>
2023-08-19 23:08:55 +0600

    Print Message in Django

--- projects/abcschool/accounts/views.py
+++ projects/abcschool/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User, auth
-
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
@@ -10,23 +10,24 @@ def register(request):
 
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
-        user_name = request.POST['user_name']
+        username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
         confirm_password = request.POST['confirm_password']
         
         if password == confirm_password:
             if User.objects.filter(email = email).exists():
-                print('Username has been taken')
-            elif User.objects.filter(user_name=user_name).exists():
-                print('email has been taken')
+                messages.info(request, 'Email has been taken')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username has been taken')
             else:
-                user = User.objects.create_user(username=user_name, password=password, confirm_password=confirm_password, email=email, first_name=first_name, last_name=last_name)
+                user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
                 user.save();
-                print('User has been created')
+                messages.info(request, 'User has been created')
         else:
-            print('password does not match')
-        return redirect('/accounts/register')
+            messages.info(request, 'password does not match')
+        return redirect('register')
     
     else:
         return render(request, 'register.html')
